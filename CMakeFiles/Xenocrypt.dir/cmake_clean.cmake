196ebc6cb0ba446bb023dbf2d4e9c2b5199472d4
gitu1997 <gitu_poke@yahoo.com>
2017-01-28 11:09:29 +0530

    Added colour thresholding code

--- CMakeFiles/Xenocrypt.dir/cmake_clean.cmake
+++ CMakeFiles/Xenocrypt.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/Xenocrypt.dir/src/Xenocrypt.cpp.o"
   "Xenocrypt.pdb"
   "Xenocrypt"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/Xenocrypt.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Xenocrypt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- CMakeFiles/learn.dir/cmake_clean.cmake
+++ CMakeFiles/learn.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/learn.dir/src/learn.cpp.o"
   "learn.pdb"
   "learn"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
